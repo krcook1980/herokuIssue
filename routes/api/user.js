b506180527dfd5f71fd6270880cf0564046e71ee
Kelly Cook <kellyrcook1980@gmail.com>
2021-05-26 13:23:48 -0600

    put route in user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -11,7 +11,7 @@ router.route('/')
 router.route("/findMatches")
     .post(userController.findMatches)
 
-router.route('/:id')
+router.route('/getUser/:id')
     .get(userController.getUser)
 
 router.route('/updateUser')    
